9b4bcba651e4a633486e262a8df2f28e0e131b8a
dimajolkin <dimajolkin@rambler.ru>
2021-07-23 23:36:47 +0300

    Added resresh page for memebers

--- include/app/page/members.h
+++ include/app/page/members.h
@@ -9,12 +9,29 @@ class MembersPage : public Page
 {
 private:
     MembersModel *model;
+    Thread *thread;
 
 public:
     MembersPage(ServiceLocator *container) : Page(container)
     {
         model = new MembersModel(container->getMemberService()->getList());
         view = new MembersView(model);
+        thread = new Thread();
+        thread->start(callback(this, &MembersPage::onTask));
+    }
+
+
+    ~MembersPage() {
+        thread->terminate();
+        delete thread;
+    }
+
+    void onTask()
+    {
+        while (true) {
+            refresh();
+            thread_sleep_for(500);
+        }
     }
 
     Response *onClick(Keyboard::KEY key)
