443e3a56769736110e1719075ec251e85b67bfad
Dimajolkin <dimajolkin@gmail.com>
2023-01-05 02:06:48 +0300

    update config

--- include/app/runtime.h
+++ include/app/runtime.h
@@ -2,74 +2,80 @@
 #include "app.h"
 #include "service-locator.h"
 
-class AppRuntime {
- private:
+class AppRuntime
+{
+private:
   ServiceLocator *container;
   App *app;
 
- public:
- // void onMembersStart()
-// {
-//   Lan *lan = container->getLan();
-//   MemberService *memberService = container->getMemberService();
+public:
+  // void onMembersStart()
+  // {
+  //   Lan *lan = container->getLan();
+  //   MemberService *memberService = container->getMemberService();
 
-//   while (true)
-//   {
-//     if (lan->available())
-//     {
-//       Package pack = lan->read();
-//       if (pack.validate())
-//       {
-//         memberService->registerPakage(&pack);
-//       }
-//     }
-//   }
-// }
+  //   while (true)
+  //   {
+  //     if (lan->available())
+  //     {
+  //       Package pack = lan->read();
+  //       if (pack.validate())
+  //       {
+  //         memberService->registerPakage(&pack);
+  //       }
+  //     }
+  //   }
+  // }
 
-    void taskReadGps() {
-      while (true) {
-        container->getGPS()->read();
-        thread_sleep_for(10);
-      }
-    }
-
-    void taskBattry() {
-      while (true) {
-        container->getServer()->setPowerLevel(container->getBattery()->level());
-        thread_sleep_for(1000);
-      }
-    }
-
-    void onKeyPressed(Keyboard::KEY key)
+  void taskReadGps()
+  {
+    while (true)
     {
+      container->getGPS()->read();
+      thread_sleep_for(10);
+    }
+  }
+
+  void taskBattry()
+  {
+    while (true)
+    {
+      container->getServer()->setPowerLevel(container->getBattery()->level());
+      thread_sleep_for(1000);
+    }
+  }
+
+  void onKeyPressed(Keyboard::KEY key)
+  {
+    // led = !led;
+    app->onClick(key);
+  }
+
+  AppRuntime(ServiceLocator *container) : container(container)
+  {
+    this->app = new App(container);
+  }
+
+  void run()
+  {
+
+    this->app->init();
+
+    Thread gpsThread;
+    gpsThread.start(callback(this, &AppRuntime::taskReadGps));
+
+    Thread batteryThread;
+    batteryThread.start(callback(this, &AppRuntime::taskBattry));
+
+    // Thread thread;
+    // thread.start(onMembersStart);
+    // irq.fall(&interruptHandler);
+
+    while (true)
+    {
+      container->getLogger()->dispatch();
       // led = !led;
-      app->onClick(key);
+      thread_sleep_for(100);
     }
-
-    AppRuntime(ServiceLocator *container): container(container)
-    {
-        this->app = new App(container);
-    }
-
-    void run() {
-     
-      this->app->init();
-  
-      Thread gpsThread;
-      gpsThread.start(callback(this, &AppRuntime::taskReadGps));
-
-      Thread batteryThread;
-      batteryThread.start(callback(this, &AppRuntime::taskBattry));
-
-      // Thread thread;
-      // thread.start(onMembersStart);
-      // irq.fall(&interruptHandler);
-
-      while (true)
-      {
-        container->getLogger()->dispatch();
-        // led = !led;
-        thread_sleep_for(100);
-      }
-    }
+  }
 };
--- src/config.h
+++ src/config.h
@@ -6,9 +6,9 @@
 
 #define RADIO_SPI_MOSI PB_15
 #define RADIO_SPI_MISO PB_14
-#define RADIO_SPI_SCK  PB_13
-#define RADIO_CE       PA_8
-#define RADIO_CSP      PA_9
+#define RADIO_SPI_SCK PB_13
+#define RADIO_CE PA_8
+#define RADIO_CSP PA_9
 
 #define EEPROM_SDA PB_9
 #define EEPROM_SCL PB_8
@@ -17,10 +17,10 @@
 #define GPO_GPS_RX PB_7
 
 #define BTN_RIGHT PA_0
-#define BTN_LEFT  PA_4
-#define BTN_UP  PC_15
-#define BTN_DOWN  PA_3
-#define BTN_OK  PC_14
+#define BTN_LEFT PA_4
+#define BTN_UP PC_15
+#define BTN_DOWN PA_3
+#define BTN_OK PC_14
 // #define BTN_KEY  PA_0
 
 #define BATTERY_PIN PA_1
@@ -28,6 +28,5 @@
 #define BATTERY_MAX_VOLTAGE 4700
 #define BATTERY_REF_VOLTAGE 4700
 #define BATTERY_DIVIDER_RATION 2
-
 
 #define BACKGROUND_COLOR ST7735_BLACK
